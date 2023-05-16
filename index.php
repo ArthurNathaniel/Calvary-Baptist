60037b9c8939f8835fec0bab8402ca05ad16ab28
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-05-16 14:31:10 -0500

    changes

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Calvary Baptist Center is a welcoming community of believers dedicated to spreading the love and teachings of Jesus Christ. Join us for worship, fellowship, and spiritual growth.">
+    <meta name="keywords" content="Calvary Baptist Center, worship, fellowship, Jesus Christ, spiritual growth">
+    <meta name="author" content="Calvary Baptist Center">
+    <title>Calvary Baptist Center</title>
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Bootstrap  CSS CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!--Lightbox CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css" />
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <section>
+        <div class="hero">
+            <div class="hero-text">
+                <h1>Welcome to Calvary Baptist Center</h1>
+                <p>
+                    Experience the love of Christ and find purpose in fellowship and service.
+                </p>
+                <button class="button-33">
+                    <a href="">About Us <i class="fas fa-info-circle"></i></a>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="home-box-">
+            <div class="box">
+                <h1> <i class="fas fa-book-open"></i></h1>
+                <h2>Our Sermons</h2>
+                <p>
+                    Join us this Sunday as we delve into the transformative power of God's love in our sermon series,
+                    'Unleashing Grace
+                </p>
+                <button class="btn-sermon">
+                    <a href="">View More <i class="fas fa-ellipsis-h"></i></a>
+                </button>
+            </div>
+            <div class="box dona">
+                <h1><i class="fas fa-hand-holding-heart"></i></h1>
+                <h2>Support Our Ministry</h2>
+                <p>
+                    Your generosity makes a difference - partner with us through your donation and help us continue spreading God's love
+                    and impacting lives.
+                </p>
+                <button class="btn-sermon">
+                    <a href="">Donate <i class="fas fa-dollar-sign"></i></a>
+                </button>
+            </div>
+            <div class="box">
+                <h1><i class="fas fa-envelope"></i></h1>
+                <h2>New Member Inquiry</h2>
+                <p>
+                    Ready to join our welcoming community? Reach out to us today with your questions and interest in becoming a part of our
+                    church family
+                </p>
+                <button class="btn-sermon">
+                    <a href="">Contact Us <i class="fas fa-phone"></i></a>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="about-home- row">
+            <div class="col-md-6 about-home-text">
+                <h5>ABOUT US</h5>
+                <h2>Our Sacred Journey: Calvary Baptist Center</h2>
+                <p>
+                    Rooted in the timeless wisdom of Scripture, Calvary Baptist Center embarks on a sacred journey of faith,
+                    where we surrender ourselves to the transformative power of God's love, faithfully seeking His guidance,
+                    cultivating authentic worship, fostering deep fellowship, and selflessly serving our community, all with
+                    a steadfast commitment to growing in Christ-likeness and spreading the Gospel to a world in need of hope
+                    and redemption."
+                </p>
+                <p>
+                    At Calvary Baptist Center, we are a diverse family of believers united in our love for Jesus Christ. With
+                    open hearts and open arms, we embrace all who seek spiritual nourishment, fostering a community of love,
+                    acceptance, and grace. Together, we discover our purpose, grow in our giftings, and make a positive impact
+                    in our church and beyond.
+                </p>
+                <button class="btn-sermon">
+                    <a href="">View More <i class="fas fa-ellipsis-h"></i></a>
+                </button>
+            </div>
+            <div class="col-md-6 church">
+
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="gallery">
+            <h1 class="text-center">Our Gallery</h1>
+        </div>
+        <div class="gallery-">
+            <a href="images/one-.jpg" data-lightbox="gallery" data-title="Calvary Baptist Center" download>
+                <div class="one-"></div>
+            </a>
+            <a href="images/two-.jpg" data-lightbox="gallery" data-title="Calvary Baptist Center" download>
+                <div class="two-"></div>
+            </a>
+            <a href="images/three-.jpg" data-lightbox="gallery" data-title="Calvary Baptist Center" download>
+                <div class="three-"></div>
+            </a>
+            <a href="images/four-.jpg" data-lightbox="gallery" data-title="Calvary Baptist Center" download>
+                <div class="four-"></div>
+            </a>
+
+        </div>
+    </section>
+
+    <!-- lightbox Script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js"></script>
+    <!-- Bootstrap JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
