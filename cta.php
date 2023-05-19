9538ad5a09ed3e8fa89c9b861a44ffb23d820e97
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-05-19 15:11:08 -0500

    changes

--- cta.php
+++ cta.php
@@ -1,6 +1,6 @@
 <div class="cta">
     <h1>Reach Out and Connect with Us</h1>
-    <p>"We would love to hear from you! Reach out to us today and let us know how we can assist you or answer any questions you may have.</p>
+    <p>We would love to hear from you! Reach out to us today and let us know how we can assist you or answer any questions you may have.</p>
     <button class="btn-sermon">
         <a href="contact.php">Contact Us <i class="fas fa-phone"></i></a>
     </button>
